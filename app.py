af117e05ac94dd3a4f5b7bdcac8f3849bb94ef67
sandeepbandi924 <bandisandeep1374@gmail.com>
2025-03-10 12:51:56 +0530

    CICD

--- app.py
+++ app.py
@@ -38,17 +38,13 @@ def index():
             alcohol =float(request.form['alcohol'])
        
          
-            # data = [fixed_acidity,volatile_acidity,citric_acid,residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide,density,pH,sulphates,alcohol]
-            # data = np.array(data).reshape(1, 11)
+            data = [fixed_acidity,volatile_acidity,citric_acid,residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide,density,pH,sulphates,alcohol]
+            data = np.array(data).reshape(1, 11)
             
-            # obj = PredictionPipeline()
-            # predict = obj.predict(data)
-            data = np.array([fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol]).reshape(1, 11)
-
             obj = PredictionPipeline()
-            prediction = obj.predict(data)
+            predict = obj.predict(data)
 
-            return render_template('results.html', prediction = str(prediction))
+            return render_template('results.html', prediction = str(predict))
 
         except Exception as e:
             print('The Exception message is: ',e)
@@ -57,29 +53,6 @@ def index():
     else:
         return render_template('index.html')
     
-# def index():
-#     if request.method == 'POST':
-#         fixed_acidity = float(request.form['fixed_acidity'])
-#         volatile_acidity = float(request.form['volatile_acidity'])
-#         citric_acid = float(request.form['citric_acid'])
-#         residual_sugar = float(request.form['residual_sugar'])
-#         chlorides = float(request.form['chlorides'])
-#         free_sulfur_dioxide = float(request.form['free_sulfur_dioxide'])
-#         total_sulfur_dioxide = float(request.form['total_sulfur_dioxide'])
-#         density = float(request.form['density'])
-#         pH = float(request.form['pH'])
-#         sulphates = float(request.form['sulphates'])
-#         alcohol = float(request.form['alcohol'])
-
-#         data = np.array([fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol]).reshape(1, 11)
-
-#         obj = PredictionPipeline()
-#         prediction = obj.predict(data)
-
-#         return render_template('index.html', prediction=prediction)
-    
-#     return render_template('index.html')
-
 
 
 
